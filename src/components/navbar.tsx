9c1d3d1efcdd98daa822fb2bec18aec021c6d2ae
Mfrekee <mfreke.bassey@outlook.com>
2024-09-21 07:55:15 +0100

    whatever

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 // import productThumbnail from '../images/image-product-3-thumbnail.jpg';
+import logo from "../images/logo.svg";
 
 type NavbarProps = {
     productQuantity: number;
@@ -30,7 +31,7 @@ const Navbar: React.FC<NavbarProps> = ({ productQuantity, setProductQuantity }) 
                     <button onClick={() => setIsOpen(!isOpen)} className="text-black mt-1 md:hidden">
                         ☰
                     </button>
-                    <img src="https://th.bing.com/th/id/R.f48ceff9ab3322d4e84ed12a44c484d1?rik=0KQ6OgL4T%2b9uCA&riu=http%3a%2f%2fwww.photo-paysage.com%2falbums%2fuserpics%2f10001%2fCascade_-15.JPG&ehk=kx1JjE9ugj%2bZvUIrjzSmcnslPc7NE1cOnZdra%2f3pJEM%3d&risl=1&pid=ImgRaw&r=0" alt="" />
+                    <img src={logo} alt="" />
                 </div>
                 <div className="hidden md:flex space-x-4">
                     <a href="#" className="text-black">Collections</a>
