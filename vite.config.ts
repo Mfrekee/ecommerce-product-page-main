ab6cb585666cafb51526dccc6a195c8289eae5c7
Mfrekee <mfreke.bassey@outlook.com>
2024-09-21 01:10:37 +0100

    made changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        open: true,
-    },
-    build: {
-        rollupOptions: {
-            external: ['/Windows.old/Users/Mfrekemfon/my codes/Front End Mentor/ecommerce-product-page-main']
-        }
-    }
+
 });
